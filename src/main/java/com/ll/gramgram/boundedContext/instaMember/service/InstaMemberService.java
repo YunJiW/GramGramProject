3284e1c74a55c5a0f357cc94ab87adfd738c7e90
YunJiW <dtroy371@gmail.com>
2024-05-22 20:47:41 +0900

    feat : member 와 instamember 관계 추가

--- src/main/java/com/ll/gramgram/boundedContext/instaMember/service/InstaMemberService.java
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/service/InstaMemberService.java
@@ -6,21 +6,26 @@ import com.ll.gramgram.boundedContext.instaMember.repository.InstaMemberReposito
 import com.ll.gramgram.boundedContext.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class InstaMemberService {
 
     private final InstaMemberRepository instaMemberRepository;
 
+    @Transactional
     public RsData<InstaMember> connect(Member member, String username, String gender) {
         if (findByUsername(username).isPresent()) {
             return RsData.of("F-5", "이미 다른 사용자와 연결되어있습니다.");
         }
 
         RsData<InstaMember> instaMemberRsData = create(username, gender);
+
+        member.addInstaMember(instaMemberRsData.getData());
 
         return instaMemberRsData;
 
@@ -29,6 +34,7 @@ public class InstaMemberService {
     private Optional<InstaMember> findByUsername(String username) {
         return instaMemberRepository.findByUsername(username);
     }
+
 
     private RsData<InstaMember> create(String username, String gender) {
         InstaMember instaMember = InstaMember.builder()
