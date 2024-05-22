ae870d171214406124b9d51092ed160b9a423424
YunJiW <dtroy371@gmail.com>
2024-05-22 20:20:07 +0900

    feat : 인스타 계정 저장  예정

--- src/main/java/com/ll/gramgram/boundedContext/instaMember/repository/InstaMemberController.java
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/repository/InstaMemberController.java
@@ -1,10 +1,14 @@
 package com.ll.gramgram.boundedContext.instaMember.repository;
 
 
+import com.ll.gramgram.base.Rq;
+import com.ll.gramgram.boundedContext.instaMember.form.ConnectForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,9 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/instaMember")
 public class InstaMemberController {
 
+    private final Rq rq;
+
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/connect")
-    public String connect(){
+    public String connect() {
         return "/user/instaMember/connect";
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/connect")
+    public String connect(@Valid ConnectForm connectForm) {
+        return rq.redirectWithMsg("/pop", "인스타그램 계정이 연결되었습니다.");
+    }
+
+
 }
