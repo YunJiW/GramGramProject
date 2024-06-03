49c427507939c0baf352660b60a1a05c229bf062
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2024-06-03 21:26:20 +0900

    Spring event (#17)
    * feat : springEvent
    
    * feat : stringEvent 추가 완료

---
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/eventListener/InstaMemberEventListener.java
@@ -0,0 +1,41 @@
+package com.ll.gramgram.boundedContext.instaMember.eventListener;
+
+
+import com.ll.gramgram.base.event.EventAfterFromInstaMemberChangeGender;
+import com.ll.gramgram.base.event.EventAfterLike;
+import com.ll.gramgram.base.event.EventAfterModifyAttractiveTypeCode;
+import com.ll.gramgram.base.event.EventBeforeCancelLike;
+import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class InstaMemberEventListener {
+    private final InstaMemberService instaMemberService;
+
+    @EventListener
+    @Transactional
+    public void listen(EventAfterModifyAttractiveTypeCode event) {
+
+        instaMemberService.whenAfterModifyAttractiveType(event.getLikeablePerson(), event.getOldAttractiveTypeCode());
+    }
+
+    @EventListener
+    @Transactional
+    public void listen(EventAfterLike event) {
+        instaMemberService.whenAfterLike(event.getLikeablePerson());
+    }
+
+    @EventListener
+    public void listen(EventBeforeCancelLike event) {
+        instaMemberService.whenBeforeCancelLike(event.getLikeablePerson());
+    }
+
+    @EventListener
+    public void listen(EventAfterFromInstaMemberChangeGender event) {
+        instaMemberService.whenAfterFromInstaMemberChangeGender(event.getInstaMember(), event.getOldGender());
+    }
+}
